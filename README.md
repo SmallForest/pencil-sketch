b423b007c1f70a58ba02683d2323666ba4f9a543
SmallForest <1032817724@qq.com>
2018-01-23 17:12:54 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## 铅笔素描效果（非原创）
+参考地址：[http://www.askaswiss.com/2016/01/how-to-create-pencil-sketch-opencv-python.html](http://www.askaswiss.com/2016/01/how-to-create-pencil-sketch-opencv-python.html)
---
+++ index.py
@@ -0,0 +1,17 @@
+import cv2
+
+img_rgb = cv2.imread("yangmi.jpg")
+img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+
+img_gray_inv = 255 - img_gray
+
+img_blur = cv2.GaussianBlur(img_gray_inv, ksize=(21, 21), sigmaX=0, sigmaY=0)
+
+def dodgeV2(image, mask):
+  return cv2.divide(image, 255-mask, scale=256)
+
+img_blend = dodgeV2(img_gray, img_blur)
+cv2.imshow("pencil sketch", img_blend)
+
+img_canvas = cv2.imread("img_canvas.jpg")
+img_blend = cv2.multiply(img_blend, img_canvas, scale=1/256)
